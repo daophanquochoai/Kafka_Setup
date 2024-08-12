7cc9acfdd8a40702ce6d0a5906d48b73948e56f9
daophanquochoai <hoai23828@mgail.com>
2024-08-12 11:44:29 +0700

    initialization

---
+++ src/main/java/com/doctorhoai/kafka_tech/KafkaTechApplication.java
@@ -0,0 +1,13 @@
+package com.doctorhoai.kafka_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doctorhoai/kafka_tech/KafkaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorhoai.kafka_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
